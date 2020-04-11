03195fb8c6346311fec5016f23d39939edbb12fe
dylanboswell <dylanboswell22@gmail.com>
2020-04-11 15:08:54 -0400

    Environment, minor BallScript changes

--- NovelLocomotionREAL/Assets/Scripts/BallScript.cs
+++ NovelLocomotionREAL/Assets/Scripts/BallScript.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
 
 public class BallScript : MonoBehaviour
 {
+    public bool grounded;
     // Start is called before the first frame update
     void Start()
     {
+        grounded = true;
         
+        Rigidbody ground;
     }
 
     // Update is called once per frame
@@ -15,4 +20,14 @@ public class BallScript : MonoBehaviour
     {
         
     }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        //if(collision.rigidbody == ground)
+        {
+
+        }
+    }
+
+
 }
